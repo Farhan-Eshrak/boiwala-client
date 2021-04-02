66eb668104e2f8c18075d756726582131c08ff7f
Farhan Eshrak <feshrakfreelancing01@gmail.com>
2021-04-02 16:07:27 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDf7e9dcmZj_ezDBzC6pK0flxa7t34V4mk",
+    authDomain: "boiwala-7e7e4.firebaseapp.com",
+    projectId: "boiwala-7e7e4",
+    storageBucket: "boiwala-7e7e4.appspot.com",
+    messagingSenderId: "246414838585",
+    appId: "1:246414838585:web:7ef0b0414001fed11ab6e8"
+};
+
+export default firebaseConfig;
